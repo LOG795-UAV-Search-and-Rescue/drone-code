cc7e9591ae3573853cafe1885c90cecd56a1253b
Max <60143006+ImprovUser@users.noreply.github.com>
2025-10-22 14:31:47 -0400

    Delete command.py

--- command.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-JETSON_IP = "192.168.x.x"  # whatever IP the Jetson has
-resp = requests.post(
-    f"http://{JETSON_IP}:8080/hotspot",
-    json={"action": "start"}
-)
-print(resp.json())
